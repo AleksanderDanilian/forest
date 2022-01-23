eccf549a19f7fe248363d939c1ab66bf84d5b0a3
AlexD <ale-danilyan@yandex.ru>
2022-01-23 08:24:27 +0300

    - add function prepare_crops - add model inference - minor changes to general view - added few descriptions to functions

--- predict_function.py
+++ predict_function.py
@@ -28,22 +28,25 @@ PLATE_AREA = 5.2 * 1.1  # дм^2
 pi = 3.14159
 
 
-def predict_timber(weights_yolov5=WEIGHTS_YOLOV5, weights_class=WEIGHTS_CLASS, img_dir=IMG_DIR, path=DETECT_DIR, conf=CONF, bbox_type='ellipse',
-                   final_wide=800, show=1):
+def predict_timber(w_length, weights_yolov5=WEIGHTS_YOLOV5, weights_class=WEIGHTS_CLASS, img_dir=IMG_DIR, path=DETECT_DIR,
+                   conf=CONF, bbox_type='ellipse', final_wide=800, show=1):
     """ Основная функция. Находит и рассчитывает площадь номера.
     Предсказывает координаты и размер бревен, рассчитывает площадь бревен.
     на вход:
     img_dir - путь к изображению
-    weighns_yolov5 - обученные веса
+    w_length - длина древесины
+    weighns_yolov5 - обученные веса yolo
+    weighns_class - обученные веса нейронки по классификации сортов древесины
+    path - куда сохраняем результаты
     conf - уверенность
     bbox_type - тип bounding box, варианты: ellipse, circle, box
     final_wide - ширина выводимого изображения
     show - визуализировать результат (boolean)
 
     на выходе:
-    resized - изображение шириной final_wide
-    bbox_list - список bounding_box (относительные координаты центра, высоты/ширины: x, y, w, h)
-    plate - координаты углов номера
+    img_edited - изображение шириной final_wide c нанесенными кругами
+    df - датафрейм со всеми данными по древесине
+    w_volume - обем древесины в кузове
     text_arr - распознанный номер
     """
 
@@ -95,8 +98,10 @@ def predict_timber(weights_yolov5=WEIGHTS_YOLOV5, weights_class=WEIGHTS_CLASS, i
     s_overall = round(sum(areas_list) / 100, 2)  # дм2 в м2
     areas_list = list(map(lambda x: round(x, 2), areas_list))
 
+    w_volume = w_length * s_overall
+
     img_edited = Image.fromarray(img, 'RGB')
-    img_edited.save(detect_dir + f'/{s_overall}_{text_arr}.png')  # change save_dir later!
+    img_edited.save(detect_dir + f'/{s_overall}_{w_volume}_{text_arr}.png')  # change save_dir later!
 
     bb_pandas = [str(i) for i in bboxes]  # по другому список не положить в 1 колонку в пандас
 
@@ -104,6 +109,6 @@ def predict_timber(weights_yolov5=WEIGHTS_YOLOV5, weights_class=WEIGHTS_CLASS, i
 
     df = pd.DataFrame(data=data,
                       columns=['diameter, cm', 'area, dm2', 'wood class', 'bbox'])
-    df.to_csv(detect_dir + f'/{s_overall}_{text_arr}.csv')  # change save_dir later!
+    df.to_csv(detect_dir + f'/{s_overall}_{w_volume}_{text_arr}.csv')  # change save_dir later!
 
-    return resized, bboxes, plate, text_arr, df, img_edited
+    return df, img_edited, w_volume, text_arr
