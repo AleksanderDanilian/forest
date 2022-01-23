51761238efef267ee9c6b644599a57cf2f7d7db7
AlexD <ale-danilyan@yandex.ru>
2022-01-23 08:14:01 +0300

    - add function prepare_crops - add model inference - minor changes to general view - added few descriptions to functions

--- helper_functions.py
+++ helper_functions.py
@@ -1,139 +1,175 @@
 import cv2
 import numpy as np
-import os 
+import os
 import pandas as pd
 import PIL
 from PIL import Image
 
 import cv2
 
-import numpy as np                               # Библиотека работы с массивами
+import numpy as np  # Библиотека работы с массивами
 import pandas as pd
-import matplotlib.pyplot as plt                  # Для отрисовки графиков
+import matplotlib.pyplot as plt  # Для отрисовки графиков
 from matplotlib.path import Path
 
 # from IPython.display import Image
 import PIL
 from PIL import Image
 
-import random                                    # Для генерации случайных чисел 
-import os                                        # Для работы с файлами 
+import random  # Для генерации случайных чисел
+import os  # Для работы с файлами
 import subprocess
 import sys
 
 import timeit
-import time 
+import time
 
-from shapely.geometry import Polygon            # Для функции площади многоугольника
+from shapely.geometry import Polygon  # Для функции площади многоугольника
 
 # Функции визуализации 
 
-BOX_COLOR = (255, 0, 0) # Red
-TEXT_COLOR = (40, 0, 0) # White
+BOX_COLOR = (255, 0, 0)  # Red
+TEXT_COLOR = (40, 0, 0)  # White
 pi = 3.1415926535
 PLATE_AREA = 5.2 * 1.1
 
+
 def visualize_bbox(img, bbox, area=0, color=BOX_COLOR, thickness=2, bbox_type='ellipse'):
-    x_cntr, y_cntr, w, h = map(int, bbox) # координаты центров и размеры рамок 
-    x_min, x_max, y_min, y_max = int(x_cntr - w/2), int(x_cntr + w/2), int(y_cntr - h/2), int(y_cntr + h/2)
+    """
+    Функция подсчета общей площади бревен
+    на вход: изображение, bbox
+    на выход: изображение с отметкой бревна и площадь бревна
+    """
+    x_cntr, y_cntr, w, h = map(int, bbox)  # координаты центров и размеры рамок
+    x_min, x_max, y_min, y_max = int(x_cntr - w / 2), int(x_cntr + w / 2), int(y_cntr - h / 2), int(y_cntr + h / 2)
     center_coordinates = (x_cntr, y_cntr)
 
-    if bbox_type=='ellipse':
-      a, b = int(w/2), int(h/2)
-      axesLength = (a, b)
-      cv2.ellipse(img, center_coordinates, axesLength, angle=0, startAngle=0, endAngle=360, color=color, thickness=thickness)  #angle=0, startAngle=0, endAngle=360, 
-    elif bbox_type=='circle':
-      radius = int(sq ** 0.5 / 2) # sq?
-      cv2.circle(img, center_coordinates, radius, color=color, thickness=thickness)
-      pass
+    if bbox_type == 'ellipse':
+        a, b = int(w / 2), int(h / 2)
+        axes_length = (a, b)
+        cv2.ellipse(img, center_coordinates, axes_length, angle=0, startAngle=0, endAngle=360, color=color,
+                    thickness=thickness)  # angle=0, startAngle=0, endAngle=360,
+    elif bbox_type == 'circle':
+        radius = int(sq ** 0.5 / 2)  # sq?
+        cv2.circle(img, center_coordinates, radius, color=color, thickness=thickness)
+        pass
     else:
-      cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color=color, thickness=thickness)
+        cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color=color, thickness=thickness)
 
-
-    # площадь бревна в квадратных дециметрах (делим площадь бревна в пикселях на площадь номера, потом умноражем на размер номера)
+    # площадь бревна в кв. дециметрах (делим площадь бревна в пикселях на площадь номера, потом умножаем
+    # на размер номера)
     # на бревнах указываем диаметр бревна
-    area_float = (w/2)*(h/2)*pi/area*PLATE_AREA # площадь бревна до округления (дм2)
-    area = str(round(area_float,1))
-    diameter = str(int(round((area_float/pi)**0.5*20,0))) # 20 = 2*10 (10 - перевод в см, 2 - вынесли за скобки sqrt)
+    area_float = (w / 2) * (h / 2) * pi / area * PLATE_AREA  # площадь бревна до округления (дм2)
+    area = str(round(area_float, 1))
+    diameter = str(
+        int(round((area_float / pi) ** 0.5 * 20, 0)))  # 10 - перевод в см, 2 - вынесли за скобки sqrt
 
-    fsc = 0.8 
-    ((text_width, text_height), _) = cv2.getTextSize(diameter, cv2.FONT_HERSHEY_SIMPLEX, fsc, 1)    
+    fsc = 0.8
+    ((text_width, text_height), _) = cv2.getTextSize(diameter, cv2.FONT_HERSHEY_SIMPLEX, fsc, 1)
     cv2.putText(
         img,
         text=diameter,
-        org=(x_cntr - int(text_width/2), y_cntr + int(0.5 * text_height)),
+        org=(x_cntr - int(text_width / 2), y_cntr + int(0.5 * text_height)),
         fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-        fontScale=fsc, 
-        color=TEXT_COLOR, 
+        fontScale=fsc,
+        color=TEXT_COLOR,
         lineType=cv2.LINE_AA,
     )
-    return img, area_float  # возвращает изображение с отметкой бревна и площадь бревна 
+    return img, area_float
 
 
 def visualize(image, bboxes, area=0, bbox_type='ellipse'):
+    """
+    Функция подсчета общей площади бревен
+    на вход: изображение
+    на выход: координаты углов и результат распознавания номера
+    """
     img = image.copy()
     bbxs = bboxes.copy()
     area_list = []  # список площадей каждого бревна
 
-    # print(img.shape, len(bbxs))
     if len(bbxs):
-      bbxs[:,[0,2]] = bbxs[:,[0,2]] * img.shape[1]
-      bbxs[:,[1,3]] = bbxs[:,[1,3]] * img.shape[0]
+        bbxs[:, [0, 2]] = bbxs[:, [0, 2]] * img.shape[1]
+        bbxs[:, [1, 3]] = bbxs[:, [1, 3]] * img.shape[0]
     for bbox in bbxs:
         img, timb_area = visualize_bbox(img, bbox, area=area, bbox_type=bbox_type)
         area_list.append(timb_area)
     return img, area_list
 
 
-# Функция распознавания номера
-# на вход: изображение
-# на выход: координаты углов и результат распознавания номера 
-
 # Import license plate recognition tools.
 
 os.chdir('/content/forest/nomeroff-net-master')
 
 from NomeroffNet.YoloV5Detector import Detector
-detector = Detector()
-detector.load()
-
 from NomeroffNet.BBoxNpPoints import NpPointsCraft, getCvZoneRGB, convertCvZonesRGBtoBGR, reshapePoints
-npPointsCraft = NpPointsCraft()
-npPointsCraft.load()
-
 from NomeroffNet.OptionsDetector import OptionsDetector
 from NomeroffNet.TextDetector import TextDetector
-
 from NomeroffNet import TextDetector
 from NomeroffNet import textPostprocessing
 
-os.chdir('/content/forest')
-
+detector = Detector()
+detector.load()
+npPointsCraft = NpPointsCraft()
+npPointsCraft.load()
 optionsDetector = OptionsDetector()
 optionsDetector.load("latest")
-
 textDetector = TextDetector.get_static_module("ru")
 textDetector.load("latest")
 
+os.chdir('/content/forest')
+
+
 def plate_detector(img):
+    """
+    Функция распознавания номера
+    на вход: изображение
+    на выход: координаты углов и результат распознавания номера
+    """
 
-  targetBoxes = detector.detect_bbox(img)
-  all_points = np.array(npPointsCraft.detect(img, targetBoxes,[5,2,0])) # координаты номера
+    target_boxes = detector.detect_bbox(img)
+    all_points = np.array(npPointsCraft.detect(img, target_boxes, [5, 2, 0]))  # координаты номера
 
-  if len(all_points):
-    all_points[:,:,0] = all_points[:,:,0] / img.shape[0]
-    all_points[:,:,1] = all_points[:,:,1] / img.shape[1]
-    
-  # cut zones
-  zones = convertCvZonesRGBtoBGR([getCvZoneRGB(img, reshapePoints(rect, 1)) for rect in all_points])
+    if len(all_points):
+        all_points[:, :, 0] = all_points[:, :, 0] / img.shape[0]
+        all_points[:, :, 1] = all_points[:, :, 1] / img.shape[1]
 
-  # predict zones attributes
-  regionIds, countLines = optionsDetector.predict(zones)
-  regionNames = optionsDetector.getRegionLabels(regionIds)
+    # cut zones
+    zones = convertCvZonesRGBtoBGR([getCvZoneRGB(img, reshapePoints(rect, 1)) for rect in all_points])
 
-  # find text with postprocessing by standart
-  textArr = textDetector.predict(zones)
-  textArr = textPostprocessing(textArr, regionNames)
+    # predict zones attributes
+    region_ids, count_lines = optionsDetector.predict(zones)
+    region_names = optionsDetector.getRegionLabels(region_ids)
 
-  return all_points, textArr
+    # find text with postprocessing by standard
+    text_arr = textDetector.predict(zones)
+    text_arr = textPostprocessing(text_arr, region_names)
 
+    return all_points, text_arr
+
+
+def prepare_crops(img_dir, bbox, save_path, resize_dim=(128, 64)):
+    """
+    функция подготавливает вырезанные bbox для
+    подачи в модель по классификации изображений
+    и сохраняет нарезки изображений в папку
+    на вход: стартовое изображение, bbox
+    на выход: numpy array, готовый для подачи в модель
+    """
+
+    image = Image.open(img_dir)
+    width, height = image.size
+    xc, yc, w, h = bbox
+
+    crop_box = [int(xc * width - w * width / 2), int(yc * height - h * height / 2), int(xc * width + w * width / 2),
+                int(yc * height + h * height / 2)]
+    cropped_image = image.crop(crop_box)
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+    cropped_image.save(save_path)
+
+    resized_image = cropped_image.resize(resize_dim)
+    resized_image = np.array(resized_image)
+    normalized_image = resized_image / 255
+
+    return normalized_image
--- predict_function.py
+++ predict_function.py
@@ -4,7 +4,8 @@ import pandas as pd
 import PIL
 from PIL import Image
 import numpy as np
-from helper_functions import plate_detector, visualize
+from helper_functions import plate_detector, visualize, prepare_crops
+from tensorflow.keras.models import load_model
 
 # путь к фото лесовоза, которое будет передано для расчета
 IMG_DIR = '/content/drive/MyDrive/AIU/Лесозаготовка/train/2.jpg'
@@ -13,11 +14,9 @@ IMG_DIR = '/content/drive/MyDrive/AIU/Лесозаготовка/train/2.jpg'
 WEIGHTS_YOLOV5 = '/content/drive/MyDrive/AIU/Лесозаготовка/веса_Yolo_Timber/best.pt'
 
 # путь к весам классификации
-WEIGHTS_CLASS = '/content/drive/MyDrive/AIU/Лесозаготовка/Веса_классификация/best.pt'
+WEIGHTS_CLASS = '/content/drive/MyDrive/AIU/Лесозаготовка/Веса_классификация/timber_CNN.h5'
 
 # путь к результатам распознавания леса (стандартная папка yolo)
-
-# os.makedirs('/content/yolov5/runs/detect')
 DETECT_DIR = '/content/yolov5/forest/runs/detect'
 
 # уверенность (объекты ниже этого порога не будут детектированы)
@@ -29,74 +28,82 @@ PLATE_AREA = 5.2 * 1.1  # дм^2
 pi = 3.14159
 
 
-def predict_timber(weights_yolov5=WEIGHTS_YOLOV5, img_dir=IMG_DIR, path=DETECT_DIR, conf=CONF, bbox_type='ellipse', final_wide=800, show=1):
-  ''' Основная функция. Находит и рассчитывает площадь номера. 
-  Предсказывает координаты и размер бревен, рассчитывает площадь бревен.
-  на вход: 
-  img_dir - путь к изображению
-  weighns_yolov5 - обученные веса
-  conf - уверенность
-  bbox_type - тип bounding box, варианты: ellipse, circle, box
-  final_wide - ширина выводимого изображения
-  show - визуализировать результат (boolean)
+def predict_timber(weights_yolov5=WEIGHTS_YOLOV5, weights_class=WEIGHTS_CLASS, img_dir=IMG_DIR, path=DETECT_DIR, conf=CONF, bbox_type='ellipse',
+                   final_wide=800, show=1):
+    """ Основная функция. Находит и рассчитывает площадь номера.
+    Предсказывает координаты и размер бревен, рассчитывает площадь бревен.
+    на вход:
+    img_dir - путь к изображению
+    weighns_yolov5 - обученные веса
+    conf - уверенность
+    bbox_type - тип bounding box, варианты: ellipse, circle, box
+    final_wide - ширина выводимого изображения
+    show - визуализировать результат (boolean)
 
-  на выходе:
-  resized - изображение шириной final_wide
-  bbox_list - список bounding_box (относительные координаты центра, высоты/ширины: x, y, w, h)
-  plate - координаты углов номера
-  textArr - распознанный номер
-  '''
+    на выходе:
+    resized - изображение шириной final_wide
+    bbox_list - список bounding_box (относительные координаты центра, высоты/ширины: x, y, w, h)
+    plate - координаты углов номера
+    text_arr - распознанный номер
+    """
 
-  # запуск предсказание yolo
-  os.system(f"python detect.py --weights {weights_yolov5} --img 640 --save-txt --conf {conf} --source {img_dir}")
-    
-  # определение папки с последним предсказанием yolo в папке /content/yolov5/runs/detect
-  detect_dir = max([os.path.join(path, dir) for dir in os.listdir(path)], key=os.path.getctime)
+    # запуск предсказание yolo
+    os.system(f"python detect.py --weights {weights_yolov5} --img 640 --save-txt --conf {conf} --source {img_dir}")
 
-  # чтение предсказанных координат
-  bbox_array = np.loadtxt(detect_dir+'/labels/' + os.listdir(detect_dir+'/labels/')[0])
+    # определение папки с последним предсказанием yolo в папке /content/yolov5/runs/detect
+    detect_dir = max([os.path.join(path, dir) for dir in os.listdir(path)], key=os.path.getctime)
 
-  image = cv2.imread(img_dir)
-  image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # чтение предсказанных координат
+    bbox_array = np.loadtxt(detect_dir + '/labels/' + os.listdir(detect_dir + '/labels/')[0])
 
-  bboxes = bbox_array[:,1:] 
+    image = cv2.imread(img_dir)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-  # функция распознавания номера
-  plate, textArr = plate_detector(image)
-    
-  # меняем изображение до подготовленных размеров
-  r = float(final_wide) / image.shape[1]
-  dim = (final_wide, int(image.shape[0] * r))
-  resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)  
+    bboxes = bbox_array[:, 1:]
 
-  if len(plate):
-    plate[:,:,0] = plate[:,:,0] * resized.shape[0]
-    plate[:,:,1] = plate[:,:,1] * resized.shape[1]
+    # функция распознавания номера
+    plate, text_arr = plate_detector(image)
 
-    pts = np.array(plate, np.int32)
+    # меняем изображение до подготовленных размеров
+    r = float(final_wide) / image.shape[1]
+    dim = (final_wide, int(image.shape[0] * r))
+    resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 
-    resized = cv2.polylines(resized, pts, True, (0, 0, 255), thickness=2)
+    if len(plate):
+        plate[:, :, 0] = plate[:, :, 0] * resized.shape[0]
+        plate[:, :, 1] = plate[:, :, 1] * resized.shape[1]
 
-  if len(plate):
-    area = Polygon(plate[0]).area # площадь номера в пикселях
-  else:
-    area = 4000 # средний размер номера
+        pts = np.array(plate, np.int32)
 
-  img, areas = visualize(resized, bboxes, area=area, bbox_type='ellipse')
+        resized = cv2.polylines(resized, pts, True, (0, 0, 255), thickness=2)
 
-  d_list = list(map(lambda x:round((x/pi)**0.5*20,2), areas)) #диаметры бревен (2*10)
-  s_overall = round(sum(areas)/100, 2) #дм2 в м2
-  areas = list(map(lambda x:round(x,2), areas))
-  
-  img_edited = Image.fromarray(img, 'RGB')
-  img_edited.save(detect_dir + f'/{s_overall}_{textArr}.png') # change save_dir later!
-  
-  bb_pandas = [str(i) for i in bboxes] # по другому список не положить в 1 колонку в пандас
+    if len(plate):
+        area = Polygon(plate[0]).area  # площадь номера в пикселях
+    else:
+        area = 4000  # средний размер номера
 
-  data = np.vstack((d_list, areas, bb_pandas)).transpose()
-  
-  df = pd.DataFrame(data = data,
-                    columns = ['diameter, cm', 'area, dm2', 'bbox'])
-  df.to_csv(detect_dir + f'/{s_overall}_{textArr}.csv') # change save_dir later!
+    img, areas_list = visualize(resized, bboxes, area=area, bbox_type='ellipse')
 
-  return resized, bboxes, plate, textArr, df, img_edited
+    model = load_model(weights_class)
+    w_class_list = []
+    for i in range(len(bboxes)):
+        cropped_img = prepare_crops(img_dir, bboxes[i], os.path.join(detect_dir, f'crops/{i}.png'))
+        w_class = np.argmax(model.predict(np.expand_dims(cropped_img, axis=0)))
+        w_class_list.append(w_class)
+
+    d_list = list(map(lambda x: round((x / pi) ** 0.5 * 20, 2), areas_list))  # диаметры бревен (2*10)
+    s_overall = round(sum(areas_list) / 100, 2)  # дм2 в м2
+    areas_list = list(map(lambda x: round(x, 2), areas_list))
+
+    img_edited = Image.fromarray(img, 'RGB')
+    img_edited.save(detect_dir + f'/{s_overall}_{text_arr}.png')  # change save_dir later!
+
+    bb_pandas = [str(i) for i in bboxes]  # по другому список не положить в 1 колонку в пандас
+
+    data = np.vstack((d_list, areas_list, w_class_list, bb_pandas)).transpose()
+
+    df = pd.DataFrame(data=data,
+                      columns=['diameter, cm', 'area, dm2', 'wood class', 'bbox'])
+    df.to_csv(detect_dir + f'/{s_overall}_{text_arr}.csv')  # change save_dir later!
+
+    return resized, bboxes, plate, text_arr, df, img_edited
