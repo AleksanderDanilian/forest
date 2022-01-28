52a6521242f3ebcadbd92f4b62859b1860cd1c04
AlexD <ale-danilyan@yandex.ru>
2022-01-28 11:10:10 +0300

    - changed w_classes and resize_dim for prepare_crop functions

--- predict_function.py
+++ predict_function.py
@@ -72,9 +72,9 @@ def predict_timber(w_length, weights_yolov5, weights_class, img_dir, path,
 
     model = load_model(weights_class)
     w_class_list = []
-    w_classes = ['1/2', '3', 'dr']  # подвиды древесины
+    w_classes = ['1/2', 'dr', '3']  # подвиды древесины (для модели EfficientNetB0)
     for i in range(len(bboxes)):
-        cropped_img = prepare_crops(img_dir, bboxes[i], os.path.join(detect_dir, 'crops'), f'{i}.png')
+        cropped_img = prepare_crops(img_dir, bboxes[i], os.path.join(detect_dir, 'crops'), f'{i}.png', resize_dim=(48, 48))
         w_class = w_classes[np.argmax(model.predict(np.expand_dims(cropped_img, axis=0)))]
         w_class_list.append(w_class)
 
