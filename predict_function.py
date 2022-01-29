e3e5486ea87f020c1cd422b7e98ef628c8ab8e38
AlexD <ale-danilyan@yandex.ru>
2022-01-29 13:45:36 +0300

    - prepare_crops (normalize - optional)

--- predict_function.py
+++ predict_function.py
@@ -75,7 +75,7 @@ def predict_timber(w_length, weights_yolov5, weights_class, img_dir, path,
     w_classes = ['1/2', '3', 'dr']  # подвиды древесины (для модели EfficientNetB0)
     for i in range(len(bboxes)):
         cropped_img = prepare_crops(img_dir, bboxes[i], os.path.join(detect_dir, 'crops'), f'{i}.png',
-                                    resize_dim=(64, 64), normalize=True)
+                                    resize_dim=(64, 64), normalize=False)
         w_class = w_classes[np.argmax(model.predict(np.expand_dims(cropped_img, axis=0)))]
         w_class_list.append(w_class)
 
